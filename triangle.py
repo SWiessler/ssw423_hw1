748b48e2c9f78ee7e435cf53402b789a5ac4f6f2
SWiessler <s.wiessler@comcast.net>
2021-09-10 10:54:26 -0400

    updated code

--- triangle.py
+++ triangle.py
@@ -20,13 +20,37 @@ def classifyTriangle(a,b,c):
         
         
     """
-    # Note: This code is completely bogus but demonstrates a few features of python
-    if a == 3 and b == 4 and c == 5:
-        return 'Right'
-    elif a == 3 and b == c:
-        return 'Scalene'
-    else:
+    # input checking
+    try:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        if a <= 0 or b <= 0 or c <= 0:
+           return 'NotATriangle' 
+    except:
         return 'NotATriangle'
+
+    # find count of each duplicate side length
+    sides = [a, b, c]
+    num_a = sides.count(a)
+    num_b = sides.count(b)
+    num_c = sides.count(c)
+
+    # Check if right
+    sides.sort()
+    # sum of any two side lengths must be greater than third length
+    if sides[0] + sides[1] <= sides[2]:
+        return "NotATriangle"
+    if sides[0] ** 2 + sides[1] ** 2 == sides[2] ** 2:
+        return 'Right'
+
+    # Determine triangle from side count
+    if num_a == 3:
+        return 'Equilateral'
+    elif (num_a or num_b) == 2:
+        return 'Isoceles'
+    elif (num_a and num_b) == 1:
+        return 'Scalene'
     
         
         
@@ -45,22 +69,38 @@ class TestTriangles(unittest.TestCase):
     def testSet1(self): # test invalid inputs
         # your tests go here.  Include as many tests as you'd like
         self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        # Wiessler Tests
+        # invalid input
+        self.assertEqual(classifyTriangle("zoinks",4,5),'NotATriangle','zoinks,4,5 is NotATriangle')
+        self.assertEqual(classifyTriangle(-1,4,4),'NotATriangle','-1,4,4 is NotATriangle')
+        self.assertEqual(classifyTriangle(0,4,4),'NotATriangle','0,4,4 is NotATriangle')
+        self.assertEqual(classifyTriangle([20],4,4),'NotATriangle','[20],4,4 is NotATriangle')
+        # sides do not equal a triangle
+        self.assertEqual(classifyTriangle(3,4,8),'NotATriangle','3,4,8 is NotATriangle')
+        # valid input
+        self.assertEqual(classifyTriangle(4,4,4),'Equilateral','4,4,4 is an Equilateral triangle')
+        self.assertEqual(classifyTriangle(10,15,20),'Scalene','10,15,30 is a Scalene triangle')
+        self.assertEqual(classifyTriangle(10.5,10.5,20),'Isoceles','10.5,10.5,30 is an Isoceles triangle')
+        # large input
+        self.assertEqual(classifyTriangle(300000,400000,500000),'Right','300000,400000,500000 is a Right triangle')
         
-    def testMyTestSet2(self): 
-        # define multiple test sets to test different aspects of the code
-        # notice that tests can have bugs too!
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
-        self.assertNotEqual(classifyTriangle(10,10,10),'Isoceles','Should be Equilateral')
-        self.assertEqual(classifyTriangle(10,15,30),'Scalene','Should be Isoceles')
-        
+
 
 if __name__ == '__main__':
     # examples of running the code
+    # runClassifyTriangle(1,2,3)
+    # runClassifyTriangle(1,1,1)
+    # runClassifyTriangle(10,15,30)
+    # runClassifyTriangle(3,4,5)
+    # runClassifyTriangle("zoinks",4,5)
+
+    # running classify triangle through main
+    # not a triangle (sum of two sides is not greater than third) 
     runClassifyTriangle(1,2,3)
-    runClassifyTriangle(1,1,1)
+    # right triangle
+    runClassifyTriangle(5, 12, 13)
     
-    unittest.main(exit=False) # this runs all of the tests - use this line if running from Spyder
-    #unittest.main(exit=True) # this runs all of the tests - use this line if running from the command line
+    unittest.main(exit=True) # this runs all of the tests - use this line if running from the command line
     
     
        
